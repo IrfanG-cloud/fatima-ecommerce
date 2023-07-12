a77e000178ec5c7d533d0173269056a955f7f250
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-07-12 06:49:39 -0700

    update

--- src/app/accessories/page.tsx
+++ src/app/accessories/page.tsx
@@ -25,7 +25,7 @@ const getProductData = async () => {
   }
 
 
-export default async function Accessories({ params }: { params: { id: string } }) {
+export default async function Accessories({ params }: { params: { slug: string } }) {
 
     const data:IProduct[]= await getProductData();
     console.log(data);
