a93256d4fc18b413e894caab10f114a7830c0f28
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-07-12 15:31:14 -0700

    update

--- src/app/accessories/page.tsx
+++ src/app/accessories/page.tsx
@@ -1,78 +1,9 @@
-// 'use client'
-
-// import React, { FC } from 'react'
-// import Link from 'next/link'
-// import Image from 'next/image'
-// import { client } from '../../lib/sanityClient'
-// import { Image as IImage } from 'sanity'
-// import { urlForImage } from '../../../sanity/lib/image'
-// import type { NextPage } from 'next'
-
-// const getProductData: NextPage = async () => {
-
-//     const res = await client.fetch(`*[_type== 'product' && category-> name== 'accessories']`)
-//     return res;
-//   }
-  
-//   interface IProduct{
-//     id:any,
-//     title:string,
-//     description:string,
-//     price: number,
-//     image: IImage,
-//     category: {
-//       name:string
-//     }
-//   }
-
-
-
-// export default async function Accessories() {
-
-//     const data:IProduct[]= await getProductData();
-//     console.log(data);
-
-//   //  const handleAddToCart = async () => {
-//   //   const res = fetch("/api/cart", {
-//   //       method: "POST",
-//   //       body:JSON.stringify({
-//   //           // product_id: item._id
-//   //       })
-//   //   })
-//   //  }
-
-//   return (
-//     <div className="w-full my-20">
-//         <div className='grid grid-cols-1 md:grid-cols-4 gap-x-4 px-6'>
-//         {data.map((item) => (
-//         <div key={item.id} className="item p-6 border my-2 border-[#cdcdcd]">
-//             <Link href={/products/+item.title}>
-//             <Image 
-//                 src={urlForImage(item.image).url()} 
-//                 alt="product" 
-//                 width={300} 
-//                 height={500} 
-//             />
-//             <p>{item.price} AED</p>
-//             <p >{item.title}</p>
-//             <p>{item.description}</p>
-//             </Link>
-//         </div>
-//         ))}
-//         </div>
-//     </div>
-//   )
-// }
-
-
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { client } from '../../lib/sanityClient'
 import { Image as IImage } from 'sanity'
 import { urlForImage } from '../../../sanity/lib/image'
-
-
 
 interface IProduct {
     id: any,
@@ -86,8 +17,6 @@ interface IProduct {
 }
 
 export default async function Accessories({ products}: { products: IProduct[] }) {
-  // const products:IProduct[] = await getProductData();
-  // console.log(products)
 
   return (
     <div className="w-full my-20">
@@ -123,9 +52,8 @@ export const getServerSideProps = async () =>{
     };
   }
   
-  return products;
-
-
+  return { props: { products } };  // Return products inside props
 }
+
 
 
