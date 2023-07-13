4227d7844a0852b7c690165f1c65b36274c9d6a1
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-07-13 03:42:34 -0700

    async update

--- src/app/accessories/page.tsx
+++ src/app/accessories/page.tsx
@@ -5,7 +5,9 @@ import { client } from '../../lib/sanityClient'
 import { Image as IImage } from 'sanity'
 import { urlForImage } from '../../../sanity/lib/image'
 
-export const getProductData =  () => {
+
+
+export const getServerSideProps = async () => {
 
     const res =  client.fetch(`*[_type== 'product' && category-> name== 'accessories']`)
   
@@ -28,14 +30,14 @@ export const getProductData =  () => {
   
   
 
-export default  function Accessories(){
-    const data: any =  getProductData();
-    console.log(data);
+export default  function Accessories( ){
+    var products: any =  getServerSideProps();
+    console.log(products);
 
   return (
     <div className="w-full my-20">
         <div className='grid grid-cols-1 md:grid-cols-4 gap-x-4 px-6'>
-        {data.map((item) => (
+        {products.map((item) => (
         <div key={item.id} className="item p-6 border my-2 border-[#cdcdcd]">
             <Link href={/products/+item.title}>
             <Image 
