d69afcdde581ebeb4e7c33776139d8d66c27140b
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-07-13 09:18:11 -0700

    women updated

--- src/app/women/[id]/page.tsx
+++ src/app/women/[id]/page.tsx
@@ -9,9 +9,10 @@ import { urlForImage } from '../../../../sanity/lib/image'
 import Link from 'next/link';
 import Amountincrease from '@/app/components/Counter';
 
-export const getProductData:any = async(param='') => {
+  const getServerSideProps:any = (param='') => {
   console.log(param)
-  const res = await client.fetch(`*[_type== 'product' && category-> name== 'women' && title=='T-shirtboys']{
+
+  const res = client.fetch(`*[_type== 'product' && category-> name== 'women' && title=='T-shirtboys']{
     title,
     description,
     price,
@@ -40,8 +41,9 @@ export default async function page({ params }: { params: { id: string }}) {
 
  let param: string = params.id;
 
-  const data= await getProductData(param);
+  const data= await getServerSideProps(param);
     console.log(data)
+    
    const handleAddToCart = async () => {
     const res = fetch("/api/cart", {
         method: "POST",
