02b79b681f951af2e51617d96c9e3135db0d3344
irfan Ghyan <46726735+IrfanG-cloud@users.noreply.github.com>
2023-07-13 06:07:08 -0700

    updated

--- src/app/accessories/page.tsx
+++ src/app/accessories/page.tsx
@@ -1,55 +1,105 @@
-import React, { FC } from 'react'
+// import React, { FC } from 'react'
+// import Link from 'next/link'
+// import Image from 'next/image'
+// import { client } from '../../lib/sanityClient'
+// import { Image as IImage } from 'sanity'
+// import { urlForImage } from '../../../sanity/lib/image'
+// import { product } from '../../../sanity/product'
+
+
+  
+//   interface IProduct{
+//     id:any,
+//     title:string,
+//     description:string,
+//     price: number,
+//     image: IImage,
+//     category: {
+//       name:string
+//     }
+//   }
+
+
+// export default async function Accessories() {
+
+//     const data:IProduct[]= await getProductData();
+//     console.log(data);
+
+
+//   return (
+//     <div className="w-full my-20">
+//         <div className='grid grid-cols-1 md:grid-cols-4 gap-x-4 px-6'>
+//         {data.map((item) => (
+//         <div key={item.id} className="item p-6 border my-2 border-[#cdcdcd]">
+//             <Link href={/products/+item.title}>
+//             <Image 
+//                 src={urlForImage(item.image).url()} 
+//                 alt="product" 
+//                 width={300} 
+//                 height={500} 
+//             />
+//             <p>{item.price} AED</p>
+//             <h3>{item.title}</h3>
+//             </Link>
+//         </div>
+//     ))}
+//         </div>
+//     </div>
+//   )
+// }
+
+// const getProductData = async () => {
+
+//   const res = await client.fetch(`*[_type== 'product' && category-> name== 'accessories']`)
+//   return res;
+// }
+
+
+import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { client } from '../../lib/sanityClient'
 import { Image as IImage } from 'sanity'
 import { urlForImage } from '../../../sanity/lib/image'
-import { product } from '../../../sanity/product'
 
-
-  
-  interface IProduct{
-    id:any,
-    title:string,
-    description:string,
+interface IProduct {
+    id: any,
+    title: string,
+    description: string,
     price: number,
     image: IImage,
     category: {
-      name:string
+        name: string
     }
-  }
+}
 
-
-export default async function Accessories() {
-
-    const data:IProduct[]= await getProductData();
-    console.log(data);
-
-
+export default function Accessories({products}: { products: IProduct[] }) {
   return (
     <div className="w-full my-20">
         <div className='grid grid-cols-1 md:grid-cols-4 gap-x-4 px-6'>
-        {data.map((item) => (
-        <div key={item.id} className="item p-6 border my-2 border-[#cdcdcd]">
-            <Link href={/products/+item.title}>
-            <Image 
-                src={urlForImage(item.image).url()} 
-                alt="product" 
-                width={300} 
-                height={500} 
-            />
-            <p>{item.price} AED</p>
-            <h3>{item.title}</h3>
-            </Link>
-        </div>
-    ))}
+            {products?.map((item) => (
+                <div key={item.id} className="item p-6 border my-2 border-[#cdcdcd]">
+                    <Link href={/products/+item.title}>
+                        <Image
+                            src={urlForImage(item.image).url()}
+                            alt="product"
+                            width={300}
+                            height={500}
+                        />
+                        <p>{item.price} AED</p>
+                        <p >{item.title}</p>
+                    </Link>
+                </div>
+            ))}
         </div>
     </div>
   )
 }
 
-const getProductData = async () => {
+export async function getServerSideProps() {
+  const products = await client.fetch(`*[_type== 'product' && category-> name== 'accessories']`);
+  
+  return { props: { products }};  // Return products inside props
 
-  const res = await client.fetch(`*[_type== 'product' && category-> name== 'accessories']`)
-  return res;
+  console.log(products)
 }
